cfafe766995b51c9986ebfce8bf3dec5b9447977
Atsukin <atsuki.tamura.2740@gmail.com>
2019-11-10 01:49:50 +0900

    add admin
    adminを追加しました。

---
+++ app/controllers/admin/blocks_controller.rb
@@ -0,0 +1,41 @@
+class Admin::BlocksController < ApplicationController
+    before_action :authenticate_admin!
+    def index
+        @blocks = Block.all
+    end
+    
+    def edit
+        @block = Block.find(params[:id])
+    end
+    
+    def update
+        @block = Block.find(params[:id])
+        if @block.update(block_params)
+            redirect_to blocks_url, notice: "テーブルを編集しました。"
+        else
+            render :edit
+        end
+    end
+    
+    def new
+        @block = Block.new
+    end
+    
+    def create
+        block = Block.new(block_params)
+        if block.save
+            redirect_to blocks_url, notice: "テーブルを登録しました。"
+        else
+            render :new
+        end
+    end
+    
+    def show
+    end
+    
+    private
+    
+    def block_params
+        params.require(:block).permit(:sheet,:kind,:total)
+    end
+end
