3fad816fcba697489c858daa8d9ab33d6a198998
Atsukin <atsuki.tamura.2740@gmail.com>
2019-11-09 18:13:44 +0900

    complete_table
    テーブルを完成させました。

--- db/migrate/20191102002647_add_column_blocks.rb
+++ db/migrate/20191102002647_add_column_blocks.rb
@@ -1,4 +1,5 @@
 class AddColumnBlocks < ActiveRecord::Migration[5.2]
   def change
+    add_column :blocks, :total, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_02_000639) do
+ActiveRecord::Schema.define(version: 2019_11_08_183153) do
 
   create_table "blocks", force: :cascade do |t|
     t.integer "sheet"
@@ -18,9 +18,10 @@ ActiveRecord::Schema.define(version: 2019_11_02_000639) do
     t.string "kind"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "total"
   end
 
-  create_table "books", force: :cascade do |t|
+  create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
