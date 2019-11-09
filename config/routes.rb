0a3bd89276c65a439d0083c9b8ed33c0fb408aeb
Atsukin <atsuki.tamura.2740@gmail.com>
2019-11-09 22:01:05 +0900

    gemfileにdeviseの追加
    gemfileにdeviseを追加しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :blocks
-  resources :bookings
+  namespace :admin do
+    resources :blocks
+    resources :bookings, only: %i(show index edit)
+  end
+  resources :bookings, only: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
